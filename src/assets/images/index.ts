9b56a6f6cb9f1fabc8c13a4d2687d51068c1bd9d
shubhkotnala <skshubhankar@gmail.com>
2022-06-04 08:10:20 +0530

    Optimised images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,20 +1,20 @@
-import intro from "./intro.png";
+import intro from "./intro.webp";
 import Aseem from "./aseemGif.gif";
 import Tanya from "./tanyaGif.gif";
-import heart from "./heart.png";
-import ring from "./ring.png";
-import specialRequest from "./specialRequest.png";
-import weddingEvents from "./weddingEvents.png";
-import saveTheDate from "./saveTheDate.png";
-import brideGroom from "./bride.png";
-import rajwadaMap from "./rajwadaMap.png";
-import pagodaMap from "./pagodaMap.png";
-import homeMap from "./homeMap.png";
-import cgrMap from "./cgrMap.png";
-import tayi from "./taya.jpg";
-import muma from "./papa.jpg";
-import divya from "./kaku.jpeg";
-import gudu from "./jiju.jpg";
+import heart from "./heart.webp";
+import ring from "./ring.webp";
+import specialRequest from "./specialRequest.webp";
+import weddingEvents from "./weddingEvents.webp";
+import saveTheDate from "./saveTheDate.webp";
+import brideGroom from "./bride.webp";
+import rajwadaMap from "./rajwadaMap.webp";
+import pagodaMap from "./pagodaMap.webp";
+import homeMap from "./homeMap.webp";
+import cgrMap from "./cgrMap.webp";
+import tayi from "./taya.webp";
+import muma from "./papa.webp";
+import divya from "./kaku.webp";
+import gudu from "./jiju.webp";
 
 
 export const imgs = {
