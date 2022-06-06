524dca124248e2659a811c45e912fff1582a7506
Samtabansal <samtabansal7@gmail.com>
2022-06-06 14:36:17 +0530

    audio test

--- src/components/heroImage/index.tsx
+++ src/components/heroImage/index.tsx
@@ -1,7 +1,24 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import { StyledContainer } from "./style";
+import song from "../../assets/mp3/song.mp3";
 
 function HeroImage() {
-  return <StyledContainer></StyledContainer>;
+  const url =
+    "https://www.youtube.com/watch?v=rtOvBOTyX00";
+
+  return (
+    <StyledContainer>
+      <iframe
+        src={require("../../assets/mp3/song.mp3")}
+        allow="autoplay"
+        id="audio"
+        style={{ display: "none" }}
+      ></iframe>
+      <audio autoPlay controls>
+        <source src={require("../../assets/mp3/song.mp3")} type="audio/mp3" />
+      </audio>
+    </StyledContainer>
+  );
 }
 
 export default HeroImage;
